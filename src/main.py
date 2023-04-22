9fbc3f9fe2c5d98fac614bf63d0bad2e5625583c
Ivan Podlipnyak <podlipnyak@econ.vsu.ru>
2023-04-22 08:59:51 +0300

    upd

--- src/main.py
+++ src/main.py
@@ -1,8 +1,8 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 import os
 from llama_index import SimpleDirectoryReader, GPTSimpleVectorIndex
 
-index_name = 'test_index'
+index_name = "test_index"
 
 def initialize_index():
     if not os.path.exists(index_name):
@@ -16,17 +16,25 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "are you lost?"
+    return jsonify({
+        "payload": "are you lost"
+    })
 
 
 @app.route("/query", methods=["GET"])
 def query_index():
+    payload = "No text found, please include a ?text=blah parameter in the URL"
+    status = 400
+
     index = GPTSimpleVectorIndex.load_from_disk(index_name)
     query_text = request.args.get("text", None)
-    if query_text is None:
-        return "No text found, please include a ?text=blah parameter in the URL", 400
-    response = index.query(query_text)
-    return str(response), 200
+    if query_text:
+        payload = index.query(query_text)
+        status = 200
+
+    response = ({ "payload": payload }, status)
+    return response
+
 
 
 if __name__ == "__main__":
